c191501726e2bb609c62ae1cda17a57a418746cd
Aijie Shu <s524256521@gmail.com>
2022-12-29 21:28:58 +0800

    Initial Commit

---
+++ popoo-ios-animation/popoo_ios_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  popoo_ios_animationApp.swift
+//  popoo-ios-animation
+//
+//  Created by Losd wind on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct popoo_ios_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
